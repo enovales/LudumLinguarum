13ef5cc05c25c74e8c908fafbf32fa30ee6af4e1
Erik Novales <enovales@codehitmen.com>
2016-01-27 09:14:26 -0800

    moved SRT functionality to LLLib

--- src/OneOffGamesPlugin/JetSetRadio.fs
+++ src/OneOffGamesPlugin/JetSetRadio.fs
@@ -29,31 +29,6 @@ type ReaderWrapper(br: BinaryReader) =
 // **************************************************************************
 // .srt subtitle files from modern re-release
 // **************************************************************************
-type SrtEntry = {
-        SubtitleId: string
-        Timecodes: string
-        Subtitle: string
-    }
-    with
-        override self.ToString() = 
-            "SrtEntry(id = " + self.SubtitleId + ", timecodes = " + self.Timecodes + ", subtitle = " + self.Subtitle + ")"
-
-let parseSrtSubtitles(lines: string array) = 
-    let unfoldNextEntry(s: string list): (SrtEntry * string list) option = 
-        let nextBlock = s |> List.takeWhile(String.IsNullOrWhiteSpace >> not)
-        match nextBlock with
-        | subtitleLine :: (timecodesLine :: subtitleText) when not(subtitleText |> List.isEmpty) -> 
-            let processedSubtitle = String.Join(" ", subtitleText |> Array.ofList).Replace(Environment.NewLine, "")
-            let nextList = s |> List.skip(nextBlock |> List.length) |> List.skipWhile(String.IsNullOrWhiteSpace)
-
-            Some(({
-                    SrtEntry.SubtitleId = subtitleLine
-                    Timecodes = timecodesLine
-                    Subtitle = processedSubtitle
-            }, nextList))
-        | _ -> None
-    Seq.unfold unfoldNextEntry (lines |> List.ofArray)
-
 
 // **************************************************************************
 // AFS archive code
---
+++ tests/LudumLinguarum.Tests/SrtToolsTests.fs
@@ -0,0 +1,101 @@
+﻿module SrtToolsTests
+
+open System
+
+open NUnit.Framework
+open SrtTools
+
+[<TestFixture>]
+type SrtToolsTests() =
+    [<TestFixtureSetUp>]
+    member this.SetUpTestFixture() = ()
+
+    [<TestFixtureTearDown>]
+    member this.TearDownTestFixture() = ()
+
+    [<SetUp>]
+    member this.SetUpTest() = ()
+
+    [<TearDown>]
+    member this.TearDownTest() = ()
+
+    [<Test>]
+    member this.TestSrtParseSingle() = 
+        let subtitleText = 
+            """1
+00:00:00,000 --> 00:00:01,000
+Test Single Line Subtitle""".Split([| Environment.NewLine; "\r"; "\n" |], StringSplitOptions.None)
+        let results = SrtTools.parseSrtSubtitles(subtitleText) |> Seq.toArray
+        let expected = [| 
+            {
+                SrtEntry.SubtitleId = "1"
+                Timecodes = "00:00:00,000 --> 00:00:01,000"
+                Subtitle = "Test Single Line Subtitle"
+            }
+        |]
+
+        Assert.AreEqual(expected, results)
+
+    [<Test>]
+    member this.TestSrtParseMultipleEntries() = 
+        let subtitleText = 
+            """1
+00:00:00,000 --> 00:00:01,000
+Test Single Line Subtitle
+
+2
+00:00:01,000 --> 00:00:02,000
+Test Single Line Subtitle 2""".Split([| Environment.NewLine; "\r"; "\n" |], StringSplitOptions.None)
+        let results = SrtTools.parseSrtSubtitles(subtitleText) |> Seq.toArray
+        let expected = [| 
+            {
+                SrtEntry.SubtitleId = "1"
+                Timecodes = "00:00:00,000 --> 00:00:01,000"
+                Subtitle = "Test Single Line Subtitle"
+            };
+            {
+                SrtEntry.SubtitleId = "2"
+                Timecodes = "00:00:01,000 --> 00:00:02,000"
+                Subtitle = "Test Single Line Subtitle 2"
+            }
+        |]
+
+        Assert.AreEqual(expected, results)
+
+    [<Test>]
+    member this.TestSrtParseLongEntry() = 
+        let subtitleText = 
+            """1
+00:00:00,000 --> 00:00:01,000
+Test Two Line Subtitle
+which is a subtitle that has two parts""".Split([| Environment.NewLine; "\r"; "\n" |], StringSplitOptions.None)
+        let results = SrtTools.parseSrtSubtitles(subtitleText) |> Seq.toArray
+        let expected = [| 
+            {
+                SrtEntry.SubtitleId = "1"
+                Timecodes = "00:00:00,000 --> 00:00:01,000"
+                Subtitle = "Test Two Line Subtitle which is a subtitle that has two parts"
+            }
+        |]
+
+        Assert.AreEqual(expected, results)
+
+    [<Test>]
+    member this.TestSrtParseMalformedEntry() = 
+        let subtitleText = 
+            """1
+00:00:00,000 --> 00:00:01,000
+Test Single Line Subtitle
+
+2
+00:00:01,000 --> 00:00:02,000""".Split([| Environment.NewLine; "\r"; "\n" |], StringSplitOptions.None)
+        let results = SrtTools.parseSrtSubtitles(subtitleText) |> Seq.toArray
+        let expected = [| 
+            {
+                SrtEntry.SubtitleId = "1"
+                Timecodes = "00:00:00,000 --> 00:00:01,000"
+                Subtitle = "Test Single Line Subtitle"
+            }
+        |]
+
+        Assert.AreEqual(expected, results)
