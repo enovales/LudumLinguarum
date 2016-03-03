b788a6c147d1fd9d64efb5d9330b651f5b9ec76d
Erik Novales <enovales@codehitmen.com>
2016-03-03 09:16:25 -0800

    reorganized a bit. added support for Skulls of the Shogun.

---
+++ src/OneOffGamesPlugin/SimpleGames.fs
@@ -0,0 +1,17 @@
+﻿module SimpleGames
+
+open LLDatabase
+open OneOffGamesUtils
+open System
+open System.IO
+
+let ExtractSkullsOfTheShogun(path: string, db: LLDatabase, g: GameRecord, args: string array) = 
+    let lessonEntry = {
+        LessonRecord.GameID = g.ID;
+        ID = 0;
+        Name = "Game Text"
+    }
+    let lessonEntryWithId = { lessonEntry with ID = db.CreateOrUpdateLesson(lessonEntry) }
+    OneOffGamesUtils.ExtractStringsFromAssemblies(path, Path.Combine(path, "SkullsOfTheShogun.exe"), "SkullsOfTheShogun.resources.dll", "TBS.Properties.AllResources", "gametext", lessonEntryWithId.ID)
+    |> Array.filter(fun t -> not(String.IsNullOrWhiteSpace(t.Text)))
+    |> db.CreateOrUpdateCards
