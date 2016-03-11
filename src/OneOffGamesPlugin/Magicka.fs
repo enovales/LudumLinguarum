f28ffcfee155cd8ef2ebd16582f449f51b5a37f0
Erik Novales <enovales@codehitmen.com>
2016-03-10 18:52:01 -0800

    wip: adding OpenXML support

--- src/OneOffGamesPlugin/Magicka.fs
+++ src/OneOffGamesPlugin/Magicka.fs
@@ -1,20 +1,27 @@
 ﻿module Magicka
 
+open DocumentFormat.OpenXml
+open DocumentFormat.OpenXml.Spreadsheet
 open LLDatabase
 open System
 open System.IO
 
+let internal getFirstTwoColumnsForWorksheet()
+
+let internal generateCardsForWorkbook(worksheetPath: string) = 
+    let workbook = new Workbook(File.ReadAllText(worksheetPath))
+    workbook.Sheets.GetEnumerator() |> 
+    [||]
+
+let internal generateCardsForLanguage(languagePath: string) = 
+
+    // TODO: map three character languages to two
+
+    Directory.GetFiles(languagePath, "*.xml")
+    |> Array.collect generateCardsForWorkbook
+
 let ExtractMagicka(path: string, db: LLDatabase, g: GameRecord, args: string array) = 
     let threeCharLanguages = Directory.GetDirectories(Path.Combine(path, @"Content\Languages"))
-
-    let generateCardsForLanguage(languagePath: string) = 
-        let generateCardsForWorksheet(worksheetPath: string) = 
-            [||]
-
-        // TODO: map three character languages to two
-
-        Directory.GetFiles(languagePath, "*.xml")
-        |> Array.collect generateCardsForWorksheet
 
     threeCharLanguages
     |> Array.collect generateCardsForLanguage
