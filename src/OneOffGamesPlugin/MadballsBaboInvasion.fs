5bc1f62e3e2a7f213eb5dbc6621507f45d592d58
Erik Novales <enovales@codehitmen.com>
2016-05-17 19:44:25 -0700

    added support for Madballs: Babo Invasion

---
+++ src/OneOffGamesPlugin/MadballsBaboInvasion.fs
@@ -0,0 +1,112 @@
+﻿module MadballsBaboInvasion
+
+open LLDatabase
+open SQLite
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type Key() =
+    [<PrimaryKey; AutoIncrement>]
+    member val IndexID: int = 0 with get, set
+    member val CatID: int = 0 with get, set
+    member val ConstID: string = "" with get, set
+    member val Context: string = "" with get, set
+    member val Value: string = "" with get, set
+
+type Value() = 
+    [<PrimaryKey; AutoIncrement>]
+    member val IndexID: int = 0 with get, set
+    member val Value: string = "" with get, set
+
+type Category() = 
+    [<PrimaryKey; AutoIncrement>]
+    member val CatID: int = 0 with get, set
+    member val Name: string = "" with get, set
+
+let private formattingRegexes = 
+    [|
+        new Regex(@"\^[0-9]+")
+        new Regex(@"\{[0-9]+?\}")
+        new Regex(@"\[.+?\]")
+    |]
+
+let internal stripFormattingTags(s: string) = 
+    Array.fold (fun (a: string)(r: Regex) -> r.Replace(a, "")) s formattingRegexes
+
+// hardcoded categories based on contents of "Category" table in each DB
+let private categories =
+    [|
+        new Category(CatID = 1, Name = "arena")
+        new Category(CatID = 2, Name = "babos")
+        new Category(CatID = 3, Name = "challenge")
+        new Category(CatID = 4, Name = "glossary")
+        new Category(CatID = 5, Name = "menus")
+        new Category(CatID = 6, Name = "errors")
+    |]
+
+let private createLessonForCategory(db: LLDatabase, gameID: int)(cat: Category) = 
+    let lessonEntry = {
+        LessonRecord.GameID = gameID;
+        ID = 0;
+        Name = cat.Name
+    }
+    { lessonEntry with ID = db.CreateOrUpdateLesson(lessonEntry) }
+
+let private createCardsForDatabase(dbPath: string, language: string, lessons: LessonRecord array) = 
+    use db = new SQLiteConnection(dbPath)
+    let keyTableID = db.CreateTable(typeof<Key>)
+    let valueTableID = db.CreateTable(typeof<Value>)
+    let categoryTableID = db.CreateTable(typeof<Category>)
+
+    let keys = db.Query<Key>("select * from Key") |> Array.ofSeq
+    let valueMap = db.Query<Value>("select * from Value") |> Seq.map (fun v -> (v.IndexID, v.Value)) |> Map.ofSeq
+
+    let makeCardForKey(k: Key) = 
+        {
+            CardRecord.ID = 0
+            LessonID = lessons.[k.CatID - 1].ID
+            Text = stripFormattingTags(valueMap.[k.IndexID])
+            Gender = "masculine"
+            Key = k.ConstID
+            GenderlessKey = k.ConstID
+            KeyHash = 0
+            GenderlessKeyHash = 0
+            SoundResource = ""
+            LanguageTag = language
+            Reversible = true
+        }
+
+    keys
+    |> Array.map makeCardForKey
+
+let ExtractMadballsBaboInvasion(path: string, db: LLDatabase, g: GameRecord, args: string array) = 
+    // each language file corresponds to a .db in path\main\db
+    let languages = 
+        [|
+            "de"
+            "en"
+            "es"
+            "fr"
+            "it"
+            "ja"
+            "ko"
+            "pt"
+            "ru"
+            "zh"
+        |]
+
+    // create a lesson for each category
+    let lessons = 
+        categories
+        |> Array.map(createLessonForCategory(db, g.ID))
+
+    let generateCardsForLanguage(l: string) = 
+        let dbPath = Path.Combine(path, @"main\db\" + l + ".db")
+        createCardsForDatabase(dbPath, l, lessons)
+
+    // read in each database, and make cards for it
+    languages
+    |> Array.collect generateCardsForLanguage
+    |> Array.filter(fun t -> not(String.IsNullOrWhiteSpace(t.Text)))
+    |> db.CreateOrUpdateCards
---
+++ tests/OneOffGamesPlugin.Tests/MadballsBaboInvasionTests.fs
@@ -0,0 +1,50 @@
+﻿module MadballsBaboInvasionTests
+
+open MadballsBaboInvasion
+open NUnit.Framework
+
+type MadballsBaboInvasionTests() = 
+    [<Test>]
+    member this.``Calling stripFormattingTags doesn't affect strings without formatting tags``() = 
+        let input = "This is a test string with no formatting tags."
+        Assert.AreEqual(input, stripFormattingTags(input))
+
+    [<Test>]
+    member this.``Calling stripFormattingTags removes tags of the format ^###``() = 
+        let input1 = "Here's a caret tag: ^123 Isn't it great?"
+        let expected = "Here's a caret tag:  Isn't it great?"
+        let input2 = "Here's a caret tag: ^123456789 Isn't it great?"
+        let input3 = "Here's a caret tag: ^1 Isn't it great?"
+        Assert.AreEqual(expected, stripFormattingTags(input1))
+        Assert.AreEqual(expected, stripFormattingTags(input2))
+        Assert.AreEqual(expected, stripFormattingTags(input3))
+
+    [<Test>]
+    member this.``Calling stripFormattingTags removes substitutions of the format {#}``() = 
+        let input = "Here's a substitution tag: {0} Isn't it great?"
+        let expected = "Here's a substitution tag:  Isn't it great?"
+        Assert.AreEqual(expected, stripFormattingTags(input))
+
+    [<Test>]
+    member this.``Brace removal is non-greedy``() = 
+        let input = "Here's one brace {12345} and then another {67890}"
+        let expected = "Here's one brace  and then another "
+        Assert.AreEqual(expected, stripFormattingTags(input))
+
+    [<Test>]
+    member this.``Calling stripFormattingTags removes all tags and substitutions``() = 
+        let input = "Here's a string with both ^456 and {9} Isn't it great?"
+        let expected = "Here's a string with both  and  Isn't it great?"
+        Assert.AreEqual(expected, stripFormattingTags(input))
+
+    [<Test>]
+    member this.``Calling stripFormattingTags removes contents inside brackets``() = 
+        let input = "Here's stuff in brackets [abc/def/ghi ^123] Isn't it great?"
+        let expected = "Here's stuff in brackets  Isn't it great?"
+        Assert.AreEqual(expected, stripFormattingTags(input))
+
+    [<Test>]
+    member this.``Bracket removal is non-greedy``() = 
+        let input = "Here's one bracket [12345] and then another [67890]"
+        let expected = "Here's one bracket  and then another "
+        Assert.AreEqual(expected, stripFormattingTags(input))
