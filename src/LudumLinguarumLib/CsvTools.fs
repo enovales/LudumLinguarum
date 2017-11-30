f086b20e384c772933a6e2502c9e51356bdd9e3c
Erik Novales <enovales@codehitmen.com>
2017-11-29 22:34:04 -0800

    updated dependencies, fixed compilation errors

--- src/LudumLinguarumLib/CsvTools.fs
+++ src/LudumLinguarumLib/CsvTools.fs
@@ -6,7 +6,7 @@ open System.IO
 
 let extractFieldsForLine(delimiter: string)(line: string): string array = 
     use sr = new StringReader(line + Environment.NewLine + line)
-    let config = new CsvHelper.Configuration.CsvConfiguration(Delimiter = delimiter)
+    let config = Configuration.Configuration(Delimiter = delimiter)
     let parser = new CsvParser(sr, config)
     match parser.Read() with
     | null -> [||]
