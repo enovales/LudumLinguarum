f619644ccaf40c2169a870262dcf2532b781c56a
Erik Novales <enovales@codehitmen.com>
2018-05-09 22:35:31 -0700

    Bump version to 0.17.2

--- src/OneOffGamesData/Properties/AssemblyInfo.cs
+++ src/OneOffGamesData/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("OneOffGamesData")]
 [assembly: AssemblyProductAttribute("LudumLinguarum")]
 [assembly: AssemblyDescriptionAttribute("Tools for extracting localized content from games, and turning them into language learning content.")]
-[assembly: AssemblyVersionAttribute("0.17.1")]
-[assembly: AssemblyFileVersionAttribute("0.17.1")]
+[assembly: AssemblyVersionAttribute("0.17.2")]
+[assembly: AssemblyFileVersionAttribute("0.17.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "OneOffGamesData";
         internal const System.String AssemblyProduct = "LudumLinguarum";
         internal const System.String AssemblyDescription = "Tools for extracting localized content from games, and turning them into language learning content.";
-        internal const System.String AssemblyVersion = "0.17.1";
-        internal const System.String AssemblyFileVersion = "0.17.1";
+        internal const System.String AssemblyVersion = "0.17.2";
+        internal const System.String AssemblyFileVersion = "0.17.2";
     }
 }
