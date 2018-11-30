8d1481bd77c0da76429f7a8d3413eea32c5be85f
Erik Novales <enovales@codehitmen.com>
2018-11-29 23:14:36 -0800

    Bump version to 1.1.0

--- src/OneOffGamesData/Properties/AssemblyInfo.cs
+++ src/OneOffGamesData/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("OneOffGamesData")]
 [assembly: AssemblyProductAttribute("LudumLinguarum")]
 [assembly: AssemblyDescriptionAttribute("Tools for extracting localized content from games, and turning them into language learning content.")]
-[assembly: AssemblyVersionAttribute("1.0.3")]
-[assembly: AssemblyFileVersionAttribute("1.0.3")]
+[assembly: AssemblyVersionAttribute("1.1.0")]
+[assembly: AssemblyFileVersionAttribute("1.1.0")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "OneOffGamesData";
         internal const System.String AssemblyProduct = "LudumLinguarum";
         internal const System.String AssemblyDescription = "Tools for extracting localized content from games, and turning them into language learning content.";
-        internal const System.String AssemblyVersion = "1.0.3";
-        internal const System.String AssemblyFileVersion = "1.0.3";
+        internal const System.String AssemblyVersion = "1.1.0";
+        internal const System.String AssemblyFileVersion = "1.1.0";
         internal const System.String AssemblyConfiguration = "Release";
     }
 }
