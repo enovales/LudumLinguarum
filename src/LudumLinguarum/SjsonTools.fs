d1a8e9059f1388b5e52386dac69e45d2e9b88bdb
Erik Novales <enovales@users.noreply.github.com>
2023-03-20 22:41:01 -0700

    added comment removal for SJSON -> JSON transformation, cleanup

--- src/LudumLinguarum/SjsonTools.fs
+++ src/LudumLinguarum/SjsonTools.fs
@@ -2,10 +2,7 @@ module SjsonTools
 
 open FParsec
 
-open System
 open System.IO
-open System.Text
-open System.Text.RegularExpressions
 
 // Tools to deal with "simplified JSON" and related files, as used by Autodesk Stingray and Bitsquid.
 //
@@ -23,111 +20,119 @@ type SjsonNode =
     | SjsonArray of List<SjsonNode>
     | SjsonObject of Map<string, SjsonNode>
 
+// Trace operator, for debugging parsers. https://www.quanttec.com/fparsec/users-guide/debugging-a-parser.html#tracing-a-parser
+let private (<!>) (p: Parser<_,_>) label : Parser<_,_> =
+    fun stream ->
+        printfn "%A: Entering %s" stream.Position label
+        let reply = p stream
+        match box reply.Result with
+        | null ->
+            printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
+        | _ ->
+            printfn "%A: Leaving %s (%A) (%A)" stream.Position label reply.Status reply.Result
+
+        reply
+
+// Module for the parser which is used to replace multi-line and single-line comments with a single space.
+module SjsonCommentStrippingGrammar =
+    type internal CommentPreprocessorNode =
+        | NonCommentBlock of string
+        | Comment
+
+    let private multiLineComment: Parser<_, Unit> = attempt (skipChar '/' .>> skipChar '*' .>> manyCharsTill anyChar (skipChar '*' .>> skipChar '/'))
+    let private lineComment: Parser<_, Unit> = attempt (skipChar '/' .>> skipChar '/' .>> skipRestOfLine false)
+    let private comment =
+        choice [
+            multiLineComment
+            lineComment
+        ] >>% Comment
+
+    let private nonComment =
+        // Note that we have to use `lookAhead` for the comment/end-of-file check, because
+        // we don't want to actually consume those characters right now. (We want to
+        // parse the comment block on its own, so we can accurately replace it with a single
+        // space.)
+        manyCharsTill anyChar (choice [lookAhead comment >>% (); eof]) |>> NonCommentBlock
+
+    let internal commentPreprocessorParser: Parser<_, Unit> =
+        manyTill (choice [comment; nonComment]) eof
+
 module SjsonGrammar =
-    open FParsec
+    let private sjsonNull: Parser<_, Unit> = stringReturn "null" SjsonNull .>> spaces
+    let private sjsonBoolTrue: Parser<_, Unit> = stringReturn "true" <| SjsonBool true .>> spaces
+    let private sjsonBoolFalse: Parser<_, Unit> = stringReturn "false" <| SjsonBool false .>> spaces
+    let private sjsonBool: Parser<_, Unit> = sjsonBoolTrue <|> sjsonBoolFalse
+    let private sjsonNumber: Parser<_, Unit> = pfloat .>> spaces |>> SjsonNumber
 
-    let (<!>) (p: Parser<_,_>) label : Parser<_,_> =
-        fun stream ->
-            printfn "%A: Entering %s" stream.Position label
-            let reply = p stream
-            match box reply.Result with
-            | null ->
-                printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
-            | _ ->
-                printfn "%A: Leaving %s (%A) (%A)" stream.Position label reply.Status reply.Result
+    // Parses any string between double quotes, with escaping support
+    let internal quotedStringLiteral: Parser<_, Unit> =
+        let str s = pstring s
+        let normalCharSnippet = manySatisfy (fun c -> c <> '\\' && c <> '"')
+        let escapedChar = str "\\" >>. (anyOf "\\\"nrt" |>> function
+                                                            | 'n' -> @"\n"
+                                                            | 'r' -> @"\r"
+                                                            | 't' -> @"\t"
+                                                            | '"' -> "\\\""
+                                                            | c   -> "\\" + string c)
+        between (str "\"") (str "\"")
+                (stringsSepBy normalCharSnippet escapedChar)
 
-            reply
+    let private sjsonString: Parser<_, Unit> = quotedStringLiteral .>> spaces |>> SjsonString
 
-    let sjsonNull: Parser<_, Unit> = stringReturn "null" SjsonNull .>> spaces <!> "sjsonNull"
-    let sjsonBoolTrue: Parser<_, Unit> = stringReturn "true" <| SjsonBool true .>> spaces <!> "sjsonBoolTrue"
-    let sjsonBoolFalse: Parser<_, Unit> = stringReturn "false" <| SjsonBool false .>> spaces <!> "sjsonBoolFalse"
-    let sjsonBool: Parser<_, Unit> = sjsonBoolTrue <|> sjsonBoolFalse <!> "sjsonBool"
-    let sjsonNumber: Parser<_, Unit> = pfloat .>> spaces |>> SjsonNumber <!> "sjsonNumber"
-
-    // Applies popen, then pchar repeatedly until pclose succeeds,
-    // returns the string in the middle
-    let manyCharsBetween popen pclose pchar = popen >>? manyCharsTill pchar pclose
-
-    // Parses any string between popen and pclose
-    let anyStringBetween popen pclose = manyCharsBetween popen pclose anyChar
-
-    // Parses any string between double quotes
-    let quotedString: Parser<_, Unit> = skipChar '"' |> anyStringBetween <| skipChar '"'
-    // is equivalent to: anyStringBetween (skipChar '"') (skipChar '"')
-
-    let sjsonString: Parser<_, Unit> = quotedString .>> spaces |>> SjsonString <!> "sjsonString"
-
-    let sjsonLiteral: Parser<_, Unit> =
+    let private sjsonLiteral: Parser<_, Unit> =
         choice [
             sjsonNull
             sjsonBool
             sjsonNumber
             sjsonString
-        ] <!> "sjsonLiteral"
+        ]
 
-    let sjsonNode, sjsonNodeRef = createParserForwardedToRef<SjsonNode, Unit>()
+    let private sjsonNode, private sjsonNodeRef = createParserForwardedToRef<SjsonNode, Unit>()
 
-    let manyContained popen pclose psep p = between popen pclose <| sepBy p psep
+    let private manyContained popen pclose psep p = between popen pclose <| sepBy p psep
 
-    let optionalCommaSeparator: Parser<_, Unit> =
+    let private optionalCommaSeparator: Parser<_, Unit> =
         choice [
             spaces >>. skipChar ',' .>> spaces
             spaces >>. lookAhead(skipNoneOf ['}'; ']'])
         ]
 
-    let sjsonArray =
+    let private sjsonArray =
         sjsonNode                       // parse JSON nodes...
         |> manyContained               // contained within...
             (skipChar '[' .>> spaces)  // opening square bracket...
             (skipChar ']' .>> spaces)  // and closing square bracket...
             optionalCommaSeparator
-        |>> SjsonArray <!> "sjsonArray"
+        |>> SjsonArray
 
-    let quotedStringLiteral: Parser<_, Unit> =
-        let normalChar = satisfy (fun c -> c <> '\\' && c <> '"')
-        let unescape c = match c with
-                         | 'n' -> '\n'
-                         | 'r' -> '\r'
-                         | 't' -> '\t'
-                         | c   -> c
-        let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> unescape)
-        between (pstring "\"") (pstring "\"")
-                (manyChars (normalChar <|> escapedChar))
-
-    let unquotedStringLiteral: Parser<_, Unit> =
+    let private unquotedStringLiteral: Parser<_, Unit> =
         many1Satisfy (function ' ' | '\t' | '\n' | '}' | ']' -> false | _ -> true)
 
-    let sjsonKeyString: Parser<_, Unit> =
+    let private sjsonKeyString: Parser<_, Unit> =
         choice [
-            quotedStringLiteral <!> "sjsonKeyString.quotedStringLiteral"
-            unquotedStringLiteral <!> "sjsonKeyString.unquotedStringLiteral"
+            quotedStringLiteral
+            unquotedStringLiteral
         ]
 
-    let sjsonProperty =
-        sjsonKeyString .>> spaces .>> skipChar '=' .>> spaces .>>. sjsonNode .>> spaces <!> "sjsonProperty"
+    let private sjsonProperty =
+        sjsonKeyString .>> spaces .>> skipChar '=' .>> spaces .>>. sjsonNode .>> spaces
 
-    let sjsonObject =
+    let private sjsonObject =
         sjsonProperty
         |> manyContained
             (skipChar '{' .>> spaces)
             (skipChar '}' .>> spaces)
             optionalCommaSeparator
         |>> Map.ofList
-        |>> SjsonObject <!> "sjsonObject"
+        |>> SjsonObject
 
     do sjsonNodeRef.Value <- choice [ sjsonObject; sjsonArray; sjsonLiteral ]
 
-    let sjsonFileScopeContainer =
-        spaces >>. manyTill (sjsonProperty .>> spaces) eof |>> Map.ofList |>> SjsonObject <!> "sjsonFileScopeContainer"
+    let internal sjsonFileScopeContainer =
+        spaces >>. manyTill (sjsonProperty .>> spaces) eof |>> Map.ofList |>> SjsonObject
 
-    let sjsonTestString = quotedString .>> spaces .>> skipChar '=' .>> spaces .>>. sjsonNode .>> spaces <!> "sjsonTestString"
-    let sjsonTestParser =
-        spaces >>. manyTill (sjsonProperty .>> spaces) eof |>> Map.ofList |>> SjsonObject <!> "sjsonTestParser"
-
-    let sjsonTestParser2 =
-        spaces >>. many (sjsonKeyString .>> spaces) <!> "sjsonTestParser2"
-
-    let rec printJson(node: SjsonNode, writer: TextWriter) =
+    // Used to output standard JSON, from the SJSON domain model.
+    let rec internal printJson(node: SjsonNode, writer: TextWriter) =
         match node with
         | SjsonNull -> writer.Write("null")
         | SjsonBool b -> writer.Write(b.ToString())
@@ -153,112 +158,29 @@ module SjsonGrammar =
             writer.WriteLine()
             writer.WriteLine("}")
 
+let stripComments(sjson: string): string =
+    let parsedSjson = run SjsonCommentStrippingGrammar.commentPreprocessorParser sjson
+    match parsedSjson with
+    | ParserResult.Failure (s, e, us) -> failwith "failed to remove comments from SJSON successfully"
+    | ParserResult.Success (result, _, _) ->
+        result
+        |> List.map (fun b ->
+            match b with
+            | SjsonCommentStrippingGrammar.NonCommentBlock s -> s
+            | SjsonCommentStrippingGrammar.Comment -> " "
+        )
+        |> String.concat ""
 
 
 let sjsonToJSON(sjson: string): string =
-    let parsedSjson = run SjsonGrammar.sjsonFileScopeContainer sjson
+    let parsedSjson = run SjsonGrammar.sjsonFileScopeContainer (stripComments sjson)
     match parsedSjson with
     | ParserResult.Failure _ ->
-        failwith "failed to parse SJSON successfully"
+        failwith "failed to parse SJSON successfully for JSON transformation"
     | ParserResult.Success (result, _, _) ->
         use sw = new StringWriter()
         SjsonGrammar.printJson(result, sw)
         sw.ToString()
     
-    (*
-    let testString = """
-    "foo" = "bar"
-    "baz" = "boo"
-    bonk = [1.0 2.0 3.0
-            4.0, 5.0]
-    goo = {
-        blargh = 3.0,
-        yoink = false
-        beep = "meep"
-    }
-    """
-    let parsedSjson = run SjsonGrammar.sjsonFileScopeContainer testString
-    printfn "%O" parsedSjson
-
-    //let testString2 = """foo bar baz blah"""
-    //let parsedSjson2 = run SjsonGrammar.sjsonTestParser2 testString2
-    //printfn "%O" parsedSjson2
-    *)
-
-(*
-open System.Text.RegularExpressions
-
-let rec parseSJSON (text: string) (result: System.Text.StringBuilder) (idx: int) =
-    let appendCommaIfNeeded (idx: int) =
-        if idx > 0 && result.Length > 0 && result.[result.Length - 1] <> '{' && result.[result.Length - 1] <> '[' &&
-           result.[result.Length - 1] <> ',' && result.[result.Length - 1] <> ':' then
-            result.Append(',') |> ignore
-        ()
-    
-    let parseKey (idx: int) =
-        let rec loop (idx: int) =
-            if idx >= text.Length || Char.IsWhiteSpace(text.[idx]) || text.[idx] = '=' then
-                idx
-            else
-                result.Append(text.[idx]) |> ignore
-                loop (idx + 1)
-        
-        result.Append('"') |> ignore
-        let nextIdx = loop idx
-        result.Append('"') |> ignore
-        nextIdx
-
-    if idx >= text.Length then
-        ()
-    else
-        match text.[idx] with
-        | '{' ->
-            appendCommaIfNeeded idx
-            result.Append('{') |> ignore
-            parseSJSON text result (idx + 1)
-        | '}' ->
-            result.Append('}') |> ignore
-            parseSJSON text result (idx + 1)
-        | '[' ->
-            appendCommaIfNeeded idx
-            result.Append('[') |> ignore
-            parseSJSON text result (idx + 1)
-        | ']' ->
-            result.Append(']') |> ignore
-            parseSJSON text result (idx + 1)
-        | '=' ->
-            result.Append(':') |> ignore
-            parseSJSON text result (idx + 1)
-        | '/' when (idx + 1 < text.Length) && (text.[idx + 1] = '/') ->
-            let nextIdx = text.IndexOf('\n', idx)
-            if nextIdx = -1 then () else parseSJSON text result (nextIdx + 1)
-        | '/' when (idx + 1 < text.Length) && (text.[idx + 1] = '*') ->
-            let nextIdx = text.IndexOf("*/", idx)
-            if nextIdx = -1 then () else parseSJSON text result (nextIdx + 2)
-        | '"' ->
-            appendCommaIfNeeded idx
-            result.Append('"') |> ignore
-            let rec parseString (idx: int) =
-                if idx >= text.Length || text.[idx] = '"' then
-                    result.Append('"') |> ignore
-                else
-                    result.Append(text.[idx]) |> ignore
-                    parseString (idx + 1)
-            parseString (idx + 1)
-            parseSJSON text result (idx + 2)
-        | c when Char.IsWhiteSpace(c) ->
-            parseSJSON text result (idx + 1)
-        | c ->
-            appendCommaIfNeeded idx
-            let nextIdx = parseKey idx
-            parseSJSON text result nextIdx
-
-let sjsonToJSON (sjson: string) =
-    let result = System.Text.StringBuilder()
-    parseSJSON sjson result 0
-    result.ToString()
-
-*)
-
 // Attempt to convert a file containing Lua data declarations into a simplified JSON file, which can then
 // be converted into standard JSON.
--- tests/LudumLinguarum.Tests/SjsonToolsTests.fs
+++ tests/LudumLinguarum.Tests/SjsonToolsTests.fs
@@ -1,14 +1,30 @@
 module SjsonToolsTests
 
-open System
-open System.Text
-
 open Expecto
+open FParsec.CharParsers
 open SjsonTools
 
 [<Tests>]
 let tests =
     testList "SjsonTools tests" [
+        testCase "quoted string literal parser works with no escaped quotes" <|
+            fun () ->
+                let input = "\"test\""
+                let expected = "test"
+                match run SjsonGrammar.quotedStringLiteral input with
+                | ParserResult.Success (result, _, _) ->
+                    Expect.equal result expected "parsed string had unexpected value"
+                | _ -> failwith "couldn't parse quoted string"
+
+        testCase "quoted string literal parser works with escaped quotes" <|
+            fun () ->
+                let input = "\"\\\"test\\\"\""
+                let expected = "\\\"test\\\""
+                match FParsec.CharParsers.run SjsonGrammar.quotedStringLiteral input with
+                | FParsec.CharParsers.ParserResult.Success (result, _, _) ->
+                    Expect.equal result expected "parsed string had unexpected value"
+                | _ -> failwith "couldn't parse quoted string"
+
         testCase "Parse a simple SJSON file into equivalent JSON" <|
             fun () ->
                 let input = "test = \"foo\""
@@ -16,10 +32,32 @@ let tests =
                 let result = sjsonToJSON input
                 Expect.equal result expected "unexpected parse result"
 
-        testCase "Parse a more complex SJSON file into equivalent JSON" <|
+        testCase "Parse escaped quotes" <|
+            fun () ->
+                let input = "test = \"\\\"foo\\\"\""
+                let expected = "{\n\"test\": \"\\\"foo\\\"\"\n}\n".ReplaceLineEndings()
+                let result = sjsonToJSON input
+                Expect.equal result expected "unexpected parse result"
+
+        testCase "Parse a SJSON file with a struct into equivalent JSON" <|
             fun () ->
                 let input = "test_struct = { foo = \"bar\" }"
                 let expected = "{\n\"test_struct\": {\n\"foo\": \"bar\"\n}\n\n}\n".ReplaceLineEndings()
                 let result = sjsonToJSON input
+                Expect.equal result expected "unexpected parse result"
+
+        testCase "Parse a SJSON file with an array into equivalent JSON" <|
+            fun () ->
+                let input = "test_array = [1 2 3 4 5]"
+                let expected = "{\n\"test_array\": [\n1,\n2,\n3,\n4,\n5\n]\n\n}\n".ReplaceLineEndings()
+                let result = sjsonToJSON input
+                Expect.equal result expected "unexpected parse result"
+
+        testCase "Replace comments with a single space" <|
+            fun () ->
+                let input = "foo = /* c-style comment */ 123\nbar = 456 // line comment\nbaz = 789".ReplaceLineEndings()
+
+                let expected = "foo =   123\nbar = 456  \nbaz = 789".ReplaceLineEndings()
+                let result = (stripComments input).ReplaceLineEndings()
                 Expect.equal result expected "unexpected parse result"
     ]
